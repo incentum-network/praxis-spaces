73be506f088dc25599a86e23b5a25c55775ea303
Bill Donahue <billd@incentum.network>
2019-07-16 19:43:14 +1200

    spaces

--- src/spaces.ts
+++ src/spaces.ts
@@ -2,11 +2,70 @@ import axios from 'axios'
 import { get } from 'lodash';
 
 let port = 5000
+let host = '127.0.0.1'
 let rootDir = '/home/bill/.spaces'
-const url = `http://127.0.0.1:${port}`
+const getUrl = () => `http://${host}:${port}`
+
+export interface Analyzer {
+  charFilters: string[]
+  class: string
+
+  // ArabicAnalyzer
+  // ArmenianAnalyzer
+  // BasqueAnalyzer
+  // BrazilianAnalyzer
+  // BulgarianAnalyzer
+  // CJKAnalyzer
+  // CatalanAnalyzer
+  // EnglishAnalyzer
+  // GermanAnalyzer
+  // StandardAnalyzer
+  // WhitespaceAnalyzer
+  stemExclusionSet?: string[]
+  stopWords?: string[]
+
+  // CollationKeyAnalyzer
+  locale?: {
+    country: string
+    language: string
+    variant: string
+  }
+
+  offsetGap?: number
+  positionIncrementGap?: number
+  tokenFilters?: string[]
+  tokenizer?: string
+}
 
 export interface SpaceField {
-  name: string
+  analyzer: Analyzer
+  dateTimeFormat: string
+  docValuesFormat?: string
+  expression?: string
+  facet?: 'yes' | 'no',
+  facetIndexFieldName?: string
+  group?: boolean
+  highlight?: boolean
+  indexAnalyzer?: Analyzer
+  indexOptions?: string
+  multiValued?: boolean
+  omitNorms?: boolean
+  postingsFormat?: string
+  search?: boolean
+  searchAnalyzer: Analyzer
+  similarity?: {
+    // DefaultSimilarity
+    class?: string
+    // BM25Similarity
+    b?: number
+    k1?: number
+  }
+  sort?: boolean
+  store?: boolean
+  storeDocValues?: boolean
+  termVectors?: string
+  tokenize?: boolean
+  type: 'atom' | 'text' | 'boolean' | 'long' | 'int' | 'double' | 'float' | 'lat_lon' | 'date_time' | 'virtual'
 }
 
 export interface UpdateDoc {
@@ -24,43 +83,50 @@ export function setPort(p: number) {
   port = p
 }
 
+export function setHost(h: string) {
+  host = h
+}
+
+export function getRootDir(space: string): string {
+  return `${rootDir}/${space}`
+}
+
 export async function createSpace(space: string): Promise<void> {
-  await axios.post(`${url}/createIndex`, {indexName: space, rootDir})
+  const result = await axios.post(`${getUrl()}/createIndex`, {indexName: space, rootDir: getRootDir(space)})
 }
 
 export async function deleteSpace(space: string): Promise<void> {
-  await axios.post(`${url}/deleteIndex`, {indexName: space})
+  await axios.post(`${getUrl()}/deleteIndex`, {indexName: space})
 }
 
 export async function commitSpace(space: string): Promise<number> {
-  const resp =   await axios.post(`${url}/commit`, {indexName: space})
+  const resp =   await axios.post(`${getUrl()}/commit`, {indexName: space})
   return resp.data.gen || 0
 }
 
 export async function startSpace(space: string, primaryGen: number = -1): Promise<void> {
-  await axios.post(`${url}/startIndex`, {indexName: space, primaryGen})
+  await axios.post(`${getUrl()}/startIndex`, {indexName: space, primaryGen})
 }
 
 export async function stopSpace(space: string): Promise<void> {
-  await axios.post(`${url}/stopIndex`, {indexName: space})
+  await axios.post(`${getUrl()}/stopIndex`, {indexName: space})
 }
 
-export async function registerFields(space: string, fields: SpaceField[]): Promise<any> {
-  const send = fields.map((field) => ({ [field.name]: field}))
-  return await axios.post(`${url}/registerFields`, { indexName: space, ...send})
+export async function registerFields(space: string, fields: Map<string, SpaceField>): Promise<any> {
+  return await axios.post(`${getUrl()}/registerFields`, { indexName: space, fields})
 }
 
 export async function deleteDocuments(space: string, field: string, values: any[]): Promise<number> {
-  const resp = await axios.post(`${url}/deleteDocuments`, { indexName: space, field, values})
+  const resp = await axios.post(`${getUrl()}/deleteDocuments`, { indexName: space, field, values})
   return resp.data.deleted
 }
 
 export async function addDocument(space: string, fields: any): Promise<void> {
-  await axios.post(`${url}/addDocument`, {indexName: space, fields})
+  await axios.post(`${getUrl()}/addDocument`, {indexName: space, fields})
 }
 
 export async function updateDocument(space: string, term: UpdateDoc, fields: any): Promise<void> {
-  await axios.post(`${url}/updateDocument`, {indexName: space, fields, term})
+  await axios.post(`${getUrl()}/updateDocument`, {indexName: space, fields, term})
 }
 
 export type Occur = 'FILTER' | 'MUST' | 'MUST_NOT' | 'SHOULD'
@@ -340,41 +406,84 @@ export async function searchSpace(space: string[], search: SpaceSearch): Promise
 }
 
 export const spaceFunctions = {
-  createSpace: async (space: string): Promise<boolean> => {
-    await createSpace(space)
-    return true
+  createSpace: async (space: string): Promise<any> => {
+    try {
+      await createSpace(space)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  startSpace: async (space: string, gen: number): Promise<number> => {
-    await startSpace(space, gen)
-    return gen
+  startSpace: async (space: string, gen: number): Promise<any> => {
+    try {
+      await startSpace(space, gen)
+      return { error: false, gen }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  stopSpace: async (space: string): Promise<boolean> => {
-    await stopSpace(space)
-    return true
+  stopSpace: async (space: string): Promise<any> => {
+    try {
+      await stopSpace(space)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  deleteSpace: async (space: string): Promise<boolean> => {
-    await deleteSpace(space)
-    return true
+  deleteSpace: async (space: string): Promise<any> => {
+    try {
+      await deleteSpace(space)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  commitSpace: async (space: string): Promise<number> => {
-    return await commitSpace(space)
+  commitSpace: async (space: string): Promise<any> => {
+    try {
+      const gen = await commitSpace(space)
+      return { error: false, gen }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  registerFields: async (space: string, fields: SpaceField[]): Promise<number> => {
-    return await registerFields(space, fields)
+  registerFields: async (space: string, fields: Map<string, SpaceField>): Promise<any> => {
+    try {
+      const ret = await registerFields(space, fields)
+      return { error: false, fields: ret }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  addDocument: async (space: string, fields: any): Promise<boolean> => {
-    await addDocument(space, fields)
-    return true
+  addDocument: async (space: string, fields: any): Promise<any> => {
+    try {
+      await addDocument(space, fields)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  deleteDocument: async (space: string, field: string, values: any): Promise<boolean> => {
-    await deleteDocuments(space, field, values)
-    return true
+  deleteDocument: async (space: string, field: string, values: any): Promise<any> => {
+    try {
+      await deleteDocuments(space, field, values)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  updateDocument: async (space: string, term: UpdateDoc, fields: any): Promise<boolean> => {
-    await updateDocument(space, term, fields)
-    return true
+  updateDocument: async (space: string, term: UpdateDoc, fields: any): Promise<any> => {
+    try {
+      await updateDocument(space, term, fields)
+      return { error: false }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
-  searchSpace: async (space: string[], search: SpaceSearch): Promise<TopDocs> => {
-    return await searchSpace(space, search)
+  searchSpace: async (space: string[], search: SpaceSearch): Promise<any> => {
+    try {
+      const docs = await searchSpace(space, search)
+      return { error: false, docs }
+    } catch (e) {
+      return { error: true, e }
+    }
   },
 }
